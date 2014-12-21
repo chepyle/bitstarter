47e8aacac390a184b3ebdedd7148930784214b23
Ubuntu <ubuntu@ip-172-31-29-189.us-west-2.compute.internal>
2014-12-21 18:26:35 +0000

    changed port to 8080

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var htmlfile = "index.html";
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-    var fs = require('fs');
 
-  response.send(fs.readFileSync('./index.html').toString());
-
+    var html=  fs.readFileSync(htmlfile).toString();
+    response.send(html);
  })
 
 app.listen(app.get('port'), function() {
