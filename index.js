1e13e043172c2f737eed994aaabae2ad1a4d5fcd
Ubuntu <ubuntu@ip-172-31-28-241.us-west-2.compute.internal>
2014-12-21 01:44:36 +0000

    added tigher call to readfilesync

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var fs = requrie('fs');
 var app = express();
-var buf  = fs.readFileSync('index.html');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
     
-  response.send(buf.toString());
+  response.send(fs.readFileSync('./index.html').toString());
 
  })
 
